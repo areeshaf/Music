b91173d380874bead171d4b237bd974dd25461bd
areeshaFatima <areeshaf7@gmail.com>
2018-10-25 23:19:02 +0530

    volume

--- app/src/main/java/com/example/hp/musicapp/MainActivity.java
+++ app/src/main/java/com/example/hp/musicapp/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.hp.musicapp;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer mediaPlayer;
+    AudioManager audioManager;
 
     public void playFunc(View view){
 
@@ -26,6 +30,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mediaPlayer=MediaPlayer.create(this,R.raw.amp);
+        audioManager=(AudioManager)getSystemService(AUDIO_SERVICE);
+
+        SeekBar volumeSeekBar=(SeekBar)findViewById(R.id.volumeSeekBar);
+
+        final int maxVolume=audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int currentVolume=audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        volumeSeekBar.setMax(maxVolume);
+        volumeSeekBar.setProgress(currentVolume);
+        volumeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                Log.i("SeekBar value",Integer.toString(i));
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,i,0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
 
     }
